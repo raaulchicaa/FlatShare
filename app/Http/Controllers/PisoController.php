8e384521aa0992e13e2095895d31e7c7eab4a55f
raaulchicaa <rauchiort@alu.edu.gva.es>
2026-02-16 17:46:57 +0100

    controller piso empezado

--- app/Http/Controllers/PisoController.php
+++ app/Http/Controllers/PisoController.php
@@ -4,50 +4,62 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use App\Models\Cuenta;
-use App\Models\Cliente;
+use App\Models\Piso;
+use App\Models\Usuario;
 
-class CuentaController extends Controller
+class PisoController extends Controller
 {
 function list() 
 {
-    $cuentas = Cuenta::all();
+    $pisos = Piso::all();
 
-    return view('cuenta.list', ['cuentas' => $cuentas]);
+    return view('piso.list', ['pisos' => $pisos]);
 }
 
 function new(Request $request) 
 {
 
-
     if ($request->isMethod('post')) {    
     $validated = $request->validate([
-    'codigo' => 'required|unique:cuentas|max:10',
-    'saldo' => 'required',
+    'nombre' => 'required|unique:pisos',
+    'ubicacion' => 'required',
+    'precio' => 'required|numeric',
+    'habitaciones' => 'required|integer',
+    'baños' => 'required|integer',
+    'metroscuadrados' => 'required|integer',
+    'casa' => 'required|boolean',
+    'planta' => 'required|integer',
     ]);
 
-    $cuenta = new Cuenta;
-    $cuenta->codigo = $request->codigo;
-    $cuenta->saldo = $request->saldo;
-    $cuenta->cliente_id = $request->cliente_id;
-    $cuenta->save();
+    $piso = new Piso;
+    $piso->nombre = $request->nombre;
+    $piso->ubicacion = $request->ubicacion;
+    $piso->precio = $request->precio;
+    $piso->habitaciones = $request->habitaciones;
+    $piso->baños = $request->baños;
+    $piso->metroscuadrados = $request->metroscuadrados;
+    $piso->casa = $request->casa;
+    $piso->planta = $request->planta;
+    $piso->usuario_id = $request->usuario_id;
+    $piso->save();
 
-    
 
-    return redirect()->route('cuenta_list')->with('status', 'Nueva cuenta '.$cuenta->codigo.' creada!');
+
+
+    return redirect()->route('piso_list')->with('status', 'Nuevo piso '.$piso->nombre.' creado!');
     }
     // si no venimos de hacer submit al formulario, tenemos que mostrar el formulario
 
-    $clientes = Cliente::all();
+    $usuarios = Usuario::all();
 
-    return view('cuenta.new', ['clientes' => $clientes]);
+    return view('piso.new', ['usuarios' => $usuarios]);
 }
 
 function delete($id) 
 { 
-    $cuenta = Cuenta::find($id);
-    $cuenta->delete();
-    return redirect()->route('cuenta_list')->with('status', 'Cuenta '.$cuenta->codigo.' eliminada!');
+    $piso = Piso::find($id);
+    $piso->delete();
+    return redirect()->route('piso_list')->with('status', 'Piso '.$piso->nombre.' eliminado!');
 }
 
 
@@ -56,38 +68,51 @@ function update(Request $request , $id)
 
     if ($request->isMethod('post')) {    
     $validated = $request->validate([
-    'codigo' => 'required',
-    Rule::unique('cuentas')->ignore($cuenta->id),
-    'max:10',
-    'saldo' => 'required',
-    ]);
 
+        'nombre' => [
+            'required',
+            Rule::unique('pisos')->ignore($id),
+        ],
+        'ubicacion' => 'required',
+        'precio' => 'required|numeric',
+        'habitaciones' => 'required|integer',
+        'baños' => 'required|integer',
+        'metroscuadrados' => 'required|integer',
+        'casa' => 'required|boolean',
+        'planta' => 'required|integer',
+    ]);
     }
 
-    $cuenta = Cuenta::find($id);
+    $piso = Piso::find($id);
 
     if ($request->isMethod('post')) {    
 
-    $cuenta->codigo = $request->codigo;
-    $cuenta->saldo = $request->saldo;
-    $cuenta->cliente_id = $request->cliente_id;
-    $cuenta->save();
+    $piso->nombre = $request->nombre;
+    $piso->ubicacion = $request->ubicacion;
+    $piso->precio = $request->precio;
+    $piso->habitaciones = $request->habitaciones;
+    $piso->baños = $request->baños;
+    $piso->metroscuadrados = $request->metroscuadrados;
+    $piso->casa = $request->casa;
+    $piso->planta = $request->planta;
+    $piso->usuario_id = $request->usuario_id;
+    $piso->save();
 
-    return redirect()->route('cuenta_list')->with('status', 'Editar cuenta '.$cuenta->codigo.' editada!');
+    return redirect()->route('piso_list')->with('status', 'Editar piso '.$piso->nombre.' editado!');
     }
 
-    $clientes = Cliente::all();
+    $usuarios = Usuario::all();
 
-    return view('cuenta.update' , ['cuenta' => $cuenta , 'clientes' => $clientes]);
+    return view('piso.update' , ['piso' => $piso , 'usuarios' => $usuarios]);
 
 }
 
 function filtro(Request $request){
     if($request->has('check')){
-        $cuentasFiltradas = Cuenta::filtroAND($request->buscador, $request->saldo);
+        $pisosFiltrados = Piso::filtroAND($request->buscador, $request->saldo);
     }else{
-        $cuentasFiltradas = Cuenta::filtroOR($request->buscador, $request->saldo);
+        $pisosFiltrados = Piso::filtroOR($request->buscador, $request->saldo);
     }
-        return view('cuenta.list', ['cuentas' => $cuentasFiltradas]);
+        return view('piso.list', ['pisos' => $pisosFiltrados]);
     }
 }
